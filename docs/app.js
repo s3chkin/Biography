e40da2c57ce279b5409245867af45d0cde60dfda
s3chkin <seckins191@gmail.com>
2022-02-17 11:29:02 +0200

    added javascript code

--- docs/app.js
+++ docs/app.js
@@ -3,3 +3,10 @@ $('li').attr('disabled', true);
 function hover() {
     document.getElementsById("hover").style.borderColor = #fd6900eb;
 }
+
+
+function ClickChange() {
+    let forMe = document.getElementById("forMe").innerHTML();
+    document.getElementById("header").innerHTML() = forMe;
+
+}
